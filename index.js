dd697eedb3f661777a3a595609b3367c2ebdb6a4
JoseAyobami <ayenijoseph45@gmail.com>
2025-01-14 11:44:13 +0100

    Add all files

---
+++ index.js
@@ -0,0 +1,12 @@
+/*let user1 = {}
+
+user1.name = "Adeniyi Bola"
+user1.age = 40
+"profile setting" = "dark, "
+user1.height = 800 
+
+let size = null
+alert(size === null) */
+
+let size;
+alert(size)
